7e000c1208b08c2542c1a5cd29345d1578bc1068
sikutisa <ilgooinkorea@gmail.com>
2025-07-08 23:15:14 +0900

    feature : add ChatHeaderUITests
    Related to : #15

---
+++ test/OpenChat.PlaygroundApp.Tests/Components/Pages/Chat/ChatHeaderUITests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+using NUnit.Framework;
+
+namespace OpenChat.PlaygroundApp.Tests.Components.Pages.Chat;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class ChatHeaderUITests : PageTest
+{
+    [SetUp]
+    public async Task Init()
+    {
+        await Page.GotoAsync("http://localhost:8080");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    }
+
+    [Test]
+    public async Task Given_Root_Page_When_Loaded_Then_Header_Is_Visible()
+    {
+        // Act
+        var title = await Page.Locator("h1").InnerTextAsync();
+
+        // Assert
+        title.ShouldBe("OpenChat.PlaygroundApp");
+    }
+
+    [TearDown]
+    public async Task CleanUp()
+    {
+        await Page.CloseAsync();
+    }
+}
+
