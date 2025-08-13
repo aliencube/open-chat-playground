aa878e01482de8119bcb6092a7f75bae051745eb
Justin Yoo <justin.yoojh@gmail.com>
2025-08-13 15:58:19 +1000

    Clean up commented lines

--- src/OpenChat.PlaygroundApp/Program.cs
+++ src/OpenChat.PlaygroundApp/Program.cs
@@ -1,11 +1,7 @@
-using System.ClientModel;
-
 using Microsoft.Extensions.AI;
 
-using OpenChat.PlaygroundApp.Components;
-
-using OpenAI;
 using OpenChat.PlaygroundApp.Abstractions;
+using OpenChat.PlaygroundApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,19 +17,6 @@ builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
 var chatClient = LanguageModelConnector.CreateChatClient(settings);
-
-// // You will need to set the endpoint and key to your own values
-// // You can do this using Visual Studio's "Manage User Secrets" UI, or on the command line:
-// //   cd this-project-directory
-// //   dotnet user-secrets set GitHubModels:Token YOUR-GITHUB-TOKEN
-// var credential = new ApiKeyCredential(builder.Configuration["GitHubModels:Token"] ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Token. See the README for details."));
-// var openAIOptions = new OpenAIClientOptions()
-// {
-//     Endpoint = new Uri("https://models.github.ai/inference")
-// };
-
-// var ghModelsClient = new OpenAIClient(credential, openAIOptions);
-// var chatClient = ghModelsClient.GetChatClient("openai/gpt-4o").AsIChatClient();
 
 builder.Services.AddChatClient(chatClient)
                 .UseFunctionInvocation()
