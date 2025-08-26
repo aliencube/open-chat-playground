290f67ecb0bb5f64ed5db9dac1cdccda30bfedd4
OkJa <nameofokja@gmail.com>
2025-08-26 23:22:49 +0900

    fix: correct argument name for Hugging Face base URL option

--- src/OpenChat.PlaygroundApp/Options/HuggingFaceArgumentOptions.cs
+++ src/OpenChat.PlaygroundApp/Options/HuggingFaceArgumentOptions.cs
@@ -33,7 +33,7 @@ public class HuggingFaceArgumentOptions : ArgumentOptions
         {
             switch (args[i])
             {
-                case "--base-url":
+                case "--baseurl":
                     if (i + 1 < args.Length)
                     {
                         this.BaseUrl = args[++i];
