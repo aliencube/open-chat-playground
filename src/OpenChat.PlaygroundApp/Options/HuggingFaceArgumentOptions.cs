44ee5cc2e841e5172b706182403aa0c8295c94a9
OkJa <nameofokja@gmail.com>
2025-08-26 19:31:54 +0900

    feat: remove ParseOptions method from HuggingFaceArgumentOptions class

--- src/OpenChat.PlaygroundApp/Options/HuggingFaceArgumentOptions.cs
+++ src/OpenChat.PlaygroundApp/Options/HuggingFaceArgumentOptions.cs
@@ -18,38 +18,4 @@ public class HuggingFaceArgumentOptions : ArgumentOptions
     /// </summary>
     public string? Model { get; set; }
 
-    /// <inheritdoc/>
-    protected override void ParseOptions(IConfiguration config, string[] args)
-    {
-        var settings = new AppSettings();
-        config.Bind(settings);
-
-        var huggingFace = settings.HuggingFace;
-
-        this.BaseUrl ??= huggingFace?.BaseUrl;
-        this.Model ??= huggingFace?.Model;
-
-        for (var i = 0; i < args.Length; i++)
-        {
-            switch (args[i])
-            {
-                case "--base-url":
-                    if (i + 1 < args.Length)
-                    {
-                        this.BaseUrl = args[++i];
-                    }
-                    break;
-
-                case "--model":
-                    if (i + 1 < args.Length)
-                    {
-                        this.Model = args[++i];
-                    }
-                    break;
-
-                default:
-                    break;
-            }
-        }
-    }
 }
