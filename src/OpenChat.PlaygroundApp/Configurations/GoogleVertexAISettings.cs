f60c60161e8de60ccae514fd966abaf21f06f87e
hxcva1 <hxcva1@gmail.com>
2025-08-29 17:33:43 +0900

    feat: Implement GoogleVertexAISettings class for GoogleVertexAI API configuration

---
+++ src/OpenChat.PlaygroundApp/Configurations/GoogleVertexAISettings.cs
@@ -0,0 +1,33 @@
+using OpenChat.PlaygroundApp.Abstractions;
+
+namespace OpenChat.PlaygroundApp.Configurations;
+
+/// <inheritdoc/>
+public partial class AppSettings
+{
+    /// <summary>
+    /// Gets or sets the <see cref="GoogleVertexAISettings"/> instance.
+    /// </summary>
+    public GoogleVertexAISettings? GoogleVertexAI { get; set; }
+}
+
+/// <summary>
+/// This represents the app settings entity for Google Vertex AI.
+/// </summary>
+public class GoogleVertexAISettings : LanguageModelSettings
+{
+    /// <summary>
+    /// Gets or sets the endpoint URL of Google Vertex AI API.
+    /// </summary>
+    public string? Endpoint { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Google Cloud API Key.
+    /// </summary>
+    public string? Token { get; set; }
+
+    /// <summary>
+    /// Gets or sets the model name of Google Vertex AI.
+    /// </summary>
+    public string? Model { get; set; }
+}
