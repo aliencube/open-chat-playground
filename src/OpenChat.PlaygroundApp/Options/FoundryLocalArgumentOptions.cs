1f4d7ba233ccea247060a71743905fe1cfcda7fe
ummjevel <mseagle2023@gmail.com>
2025-09-02 22:41:57 +0900

    add foundrylocal argument options

--- src/OpenChat.PlaygroundApp/Options/FoundryLocalArgumentOptions.cs
+++ src/OpenChat.PlaygroundApp/Options/FoundryLocalArgumentOptions.cs
@@ -1,4 +1,5 @@
 using OpenChat.PlaygroundApp.Abstractions;
+using OpenChat.PlaygroundApp.Configurations;
 
 namespace OpenChat.PlaygroundApp.Options;
 
@@ -7,4 +8,35 @@ namespace OpenChat.PlaygroundApp.Options;
 /// </summary>
 public class FoundryLocalArgumentOptions : ArgumentOptions
 {
+    /// <summary>
+    /// Gets or sets the alias of Foundry Local.
+    /// </summary>
+    public string? Alias { get; set; }
+
+    /// <inheritdoc/>
+    protected override void ParseOptions(IConfiguration config, string[] args)
+    {
+        var settings = new AppSettings();
+        config.Bind(settings);
+
+        var foundryLocal = settings.FoundryLocal;
+
+        this.Alias ??= foundryLocal?.Alias;
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "--alias":
+                    if (i + 1 < args.Length)
+                    {
+                        this.Alias = args[++i];
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
 }
