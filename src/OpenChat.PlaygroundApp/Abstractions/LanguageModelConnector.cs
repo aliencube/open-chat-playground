da7db3cde4bedf35bac5232e546a2f9527980f1e
gngsn <enfj.dev@gmail.com>
2025-09-07 15:34:45 +0900

    feat: remove commented-out Azure AI Foundry connector code from LanguageModelConnector

--- src/OpenChat.PlaygroundApp/Abstractions/LanguageModelConnector.cs
+++ src/OpenChat.PlaygroundApp/Abstractions/LanguageModelConnector.cs
@@ -31,7 +31,6 @@ public abstract class LanguageModelConnector(LanguageModelSettings? settings)
         LanguageModelConnector connector = settings.ConnectorType switch
         {
             ConnectorType.GitHubModels => new GitHubModelsConnector(settings),
-            // ConnectorType.AzureAIFoundry => new AzureAIFoundryConnector(settings),
             _ => throw new NotSupportedException($"Connector type '{settings.ConnectorType}' is not supported.")
         };
 
