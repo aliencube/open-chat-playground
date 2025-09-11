86b50b16c8714bfc6c8bbddfbce2c21b46217216
gngsn <enfj.dev@gmail.com>
2025-09-11 21:21:22 +0900

    Revert "refactor: Simplify credential and endpoint initialization in GitHubModelsConnector"
    This reverts commit cdae0eef75997df6a592095e8b0a0f0a797766de.

--- src/OpenChat.PlaygroundApp/Connectors/GitHubModelsConnector.cs
+++ src/OpenChat.PlaygroundApp/Connectors/GitHubModelsConnector.cs
@@ -46,10 +46,10 @@ public class GitHubModelsConnector(AppSettings settings) : LanguageModelConnecto
     {
         var settings = this.Settings as GitHubModelsSettings;
 
-        var credential = new ApiKeyCredential(settings?.Token!);
+        var credential = new ApiKeyCredential(settings?.Token ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Token."));
         var options = new OpenAIClientOptions()
         {
-            Endpoint = new Uri(settings.Endpoint!)
+            Endpoint = new Uri(settings.Endpoint ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Endpoint."))
         };
 
         var client = new OpenAIClient(credential, options);
