c6e32c8fa820f43c6c1d8179f04eec7c08d65198
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-09-23 23:31:48 +0900

    test: Replaces `Expect` assertions with `Page.WaitForSelectorAsync()`

--- test/OpenChat.PlaygroundApp.Tests/Components/Pages/Chat/ChatStreamingUITest.cs
+++ test/OpenChat.PlaygroundApp.Tests/Components/Pages/Chat/ChatStreamingUITest.cs
@@ -55,24 +55,21 @@ public class ChatStreamingUITest : PageTest
     public async Task Given_UserMessage_When_SendButton_Clicked_Then_LoadingSpinner_Should_Be_Visible_Before_Text_Arrives(string userMessage)
     {
         // Arrange
-        const int timeoutMs = 5000;
-
         const string spinnerSelector = ".lds-ellipsis";
 
         var textArea = Page.GetByRole(AriaRole.Textbox, new() { Name = "User Message Textarea" });
         var sendButton = Page.GetByRole(AriaRole.Button, new() { Name = "User Message Send Button" });
-        var spinner = Page.Locator(spinnerSelector);
+        var loadingSpinner = Page.Locator(spinnerSelector);
 
         // Act
         await textArea.FillAsync(userMessage);
         await sendButton.ClickAsync();
 
         // Assert
-        await Expect(spinner).ToBeVisibleAsync(new() { Timeout = timeoutMs });
-        
-        var spinnerWasVisible = await spinner.IsVisibleAsync();
-        
-        spinnerWasVisible.ShouldBeTrue();
+        await Page.WaitForSelectorAsync(spinnerSelector, new() { State = WaitForSelectorState.Visible });
+
+        var spinnerVisible = await loadingSpinner.IsVisibleAsync();
+        spinnerVisible.ShouldBeTrue();
     }
 
     [Trait("Category", "IntegrationTest")]
@@ -83,14 +80,12 @@ public class ChatStreamingUITest : PageTest
     public async Task Given_UserMessage_When_Response_Text_Arrives_Then_LoadingSpinner_Should_Disappear(string userMessage)
     {
         // Arrange
-        const int timeoutMs = 5000;
-
         const string spinnerSelector = ".lds-ellipsis";
         const string messageSelector = ".assistant-message-text";
 
         var textArea = Page.GetByRole(AriaRole.Textbox, new() { Name = "User Message Textarea" });
         var sendButton = Page.GetByRole(AriaRole.Button, new() { Name = "User Message Send Button" });
-        var spinner = Page.Locator(spinnerSelector);
+        var loadingSpinner = Page.Locator(spinnerSelector);
         var message = Page.Locator(messageSelector);
 
         // Act
@@ -98,11 +93,11 @@ public class ChatStreamingUITest : PageTest
         await sendButton.ClickAsync();
 
         // Assert
-        await Expect(message).Not.ToHaveTextAsync(string.Empty, new() { Timeout = timeoutMs }); 
+        var messageContent = await message.InnerTextAsync();
+        messageContent.ShouldNotBeEmpty();
 
-        var spinnerWasVisible = await spinner.IsVisibleAsync();
-
-        spinnerWasVisible.ShouldBeFalse();
+        var spinnerVisible = await loadingSpinner.IsVisibleAsync();
+        spinnerVisible.ShouldBeFalse();
     }
 
     public override async Task DisposeAsync()
