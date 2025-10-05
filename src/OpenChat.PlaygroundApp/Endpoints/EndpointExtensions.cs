f483ec98af5d60ad0794d7f01cc2266edfcd9972
Justin Yoo <justin.yoojh@gmail.com>
2025-10-05 14:05:29 +0900

    Add endpoint as dependencies

---
+++ src/OpenChat.PlaygroundApp/Endpoints/EndpointExtensions.cs
@@ -0,0 +1,52 @@
+using System.Reflection;
+
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace OpenChat.PlaygroundApp.Endpoints;
+
+/// <summary>
+/// This represents the extension entity for handling endpoints.
+/// </summary>
+public static class EndpointExtensions
+{
+    /// <summary>
+    /// Adds the chat client to the service collection.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> instance.</param>
+    /// <param name="chatClient"><see cref="IChatClient"/> instance.</param>
+    /// <returns>Returns <see cref="IServiceCollection"/> instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="services"/> is <c>null</c>.</exception>
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        var descriptors = assembly.DefinedTypes
+                                  .Where(type => type is { IsAbstract: false, IsInterface: false }
+                                              && type.IsAssignableTo(typeof(IEndpoint)))
+                                  .Select(type => ServiceDescriptor.Scoped(typeof(IEndpoint), type));
+        services.TryAddEnumerable(descriptors);
+
+        return services;
+    }
+
+    /// <summary>
+    /// Maps all the registered endpoints.
+    /// </summary>
+    /// <param name="app"><see cref="WebApplication"/> instance.</param>
+    /// <param name="group"><see cref="RouteGroupBuilder"/> instance.</param>
+    /// <returns>Returns <see cref="IApplicationBuilder"/> instance.</returns>
+    public static IApplicationBuilder MapEndpoints(this WebApplication app, RouteGroupBuilder? group = default)
+    {
+        IEndpointRouteBuilder builder = group is null ? app : group;
+
+        var endpoints = app.Services
+                           .GetRequiredService<IServiceScopeFactory>()
+                           .CreateScope()
+                           .ServiceProvider
+                           .GetRequiredService<IEnumerable<IEndpoint>>();
+        foreach (var endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(builder);
+        }
+
+        return app;
+    }
+}
